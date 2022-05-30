d5c7f95abdce2145db7109d201ef8d873ddcb527
JiZiQian <99163728+JiZiQian@users.noreply.github.com>
2022-05-30 10:46:11 +0800

    Add files via upload

--- upload/listen.js
+++ upload/listen.js
@@ -1,20 +1,54 @@
 let http=require("http");
 let fs=require("fs");
 
-function Server(req,res){
-    //设置返回网络请求header
-    res.writeHead(200,{
-        "Content-Type": "application/json;charset=utf-8",//内容类型，json格式
-        "Access-Control-Allow-Methods": "*",//允许请求方式，所有方式（*）
-        "Access-Control-Allow-Origin": "*",//允许请求来源，所有来源（*）
-    });
-    //获取文件
-    
-    //返回
-    res.end("{}");
+function fileName(str){
+    let pos=str.indexOf("filename")+7+3;
+    let ans="";
+    while(str[pos]!=="\""){
+        ans+=str[pos];
+        pos++;
+    }
+    return ans;
+}
+function getFile(str){
+    return str;
 }
 
-//创建服务
-http.createServer(Server).listen(2000);
+http.createServer(function(req,res){
+    res.writeHead(200,{
+        "Content-Type": "text/plain;charset=utf-8"
+    });
+    if(req.method==="POST"){
+        let data=[];
+        let buffer=Buffer.alloc(0);
+        req.on("data",function(chunk){
+            data.push(chunk);
+        });
+        req.on("end",function(){
+            buffer=Buffer.concat(data);
+            /*console.log(buffer.toString("utf8"));
+            res.end();*/
+            //console.log(buffer);
+            let filename=fileName(buffer.toString("utf8"));
+            buffer=getFile(buffer);
+            console.log(buffer);
+            fs.readFile("/home/jzq/home/load_file/"+filename,function(err,data){
+                if(err){
+                    fs.writeFile("/home/jzq/home/load_file/"+filename,buffer,function(err){
+                        if(!err){
+                            res.end("成功");
+                        }
+                        else{
+                            res.end(err);
+                        }
+                    });
+                }
+                else{
+                    res.end("该文件已存在，请更换文件名称");
+                }
+            });
+        });
+    }
+}).listen(2000);
 
-console.log("Accepted,listen at port 2000");
+console.log("Accepted, listen at 2000");
